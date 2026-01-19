695bbfbcf6e3fa892ed137450589018a8c292af4
Rajesh1st <rajeshtb55@outlook.com>
2026-01-19 19:43:08 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,11 @@
+API_ID = 123456
+API_HASH = "API_HASH"
+BOT_TOKEN = "BOT_TOKEN"
+
+MONGO_URL = "mongodb+srv://...."
+DB_NAME = "filestore"
+
+ADMINS = [123456789]
+SUDO_USERS = [987654321]
+
+FORCE_CHANNEL = "@your_vote_channel"
